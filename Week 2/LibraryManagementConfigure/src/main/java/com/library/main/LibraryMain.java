a74a949a0030e265df40cc1fc238e17377be49e9
abishekkumar2425-gh <abishekkumar2425@gmail.com>
2026-07-04 02:11:37 +0530

    Updated Week 2 project and removed old files

---
+++ Week 2/LibraryManagementConfigure/src/main/java/com/library/main/LibraryMain.java
@@ -0,0 +1,10 @@
+package com.library.main;
+
+public class LibraryMain {
+
+	public static void main(String[] args) {
+		 System.out.println("Maven Project has been created and Configured");
+
+	}
+
+}
