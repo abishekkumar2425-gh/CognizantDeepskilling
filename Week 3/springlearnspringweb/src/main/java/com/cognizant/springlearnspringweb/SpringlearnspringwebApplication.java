ff6892572ac054aa351551493be22399a210d3dd
abishekkumar2425-gh <abishekkumar2425@gmail.com>
2026-07-24 02:37:30 +0530

    Spring Web

---
+++ Week 3/springlearnspringweb/src/main/java/com/cognizant/springlearnspringweb/SpringlearnspringwebApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.springlearnspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearnspringwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlearnspringwebApplication.class, args);
+    }
+}
---
+++ Week 3/springlearnspringweb/src/test/java/com/cognizant/springlearnspringweb/SpringlearnspringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearnspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearnspringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
